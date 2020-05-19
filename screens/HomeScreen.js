f3b19e3e1afa79f9511c0c1dc4cba88e3b338e36
Harpreet Singh <hsit18@gmail.com>
2020-05-19 22:47:46 +0530

    categories API call implemented

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,21 @@
-import * as React from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 const HomeScreen = () => {
+  const [categories, setCategories] = useState();
+  useEffect(() => {
+    fetch("https://evening-tor-29203.herokuapp.com/api/v1/categories")
+      .then((res) => res.json())
+      .then((res) => {
+        setCategories(res.categories || []);
+      });
+  }, []);
   return (
     <View style={styles.container}>
-      <Text>Home Page Content</Text>
+      <Text>Categories</Text>
+      {categories.map((c) => (
+        <Text>{c.name}</Text>
+      ))}
     </View>
   );
 };
